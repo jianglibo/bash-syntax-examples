b01ea9705dfad66e48ed3ed4a5e8e7b991b1d56c
jianglibo <jianglibo@gmail.com>
2024-02-03 12:59:15 +0800

    az sp. !pack!

--- src/cats/bash/az.sh
+++ src/cats/bash/az.sh
@@ -1,3 +1,9 @@
 ### az azure-cli
 
 az login --service-principal -u <app-id> -p <password-or-cert> --tenant <tenant>
+
+# https://learn.microsoft.com/en-us/azure/role-based-access-control/built-in-roles
+# roles: reader, contributor, owner etc.
+az ad sp create-for-rbac --name myServicePrincipalName1 \
+                         --role reader \
+                         --scopes /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myRG1
