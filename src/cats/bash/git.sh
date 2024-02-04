2bb381c749100131dff99327c7d7b75067d533aa
jianglibo <jianglibo@gmail.com>
2024-02-04 17:41:05 +0800

    Fix git.sh script and add pull commands !pack!

--- src/cats/bash/git.sh
+++ src/cats/bash/git.sh
@@ -3,3 +3,8 @@
 # get your person token from https://github.com/settings/personal-access-tokens/new
 # permissions: content read, write (if you want to push)
 git clone https://username:github_pat_11AA@https://github.com/jianglibo/syntax-examples.git
+
+# fatal: Not possible to fast-forward, aborting.
+# If you're unsure, it's a good practice to communicate with your team before using git pull --rebase, especially on branches that are shared with others.
+git pull --rebase
+git pull --no-ff
